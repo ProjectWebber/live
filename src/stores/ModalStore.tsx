25c3e72281624c39f62f54ffcaf0a64b4dd8c5e5
ProjectWebber <webberop@gmail.com>
2026-02-22 21:04:17 -0300

    Tela temporária de 'fora do ar' por conta do Dia D (22/02/2026).

--- src/stores/ModalStore.tsx
+++ src/stores/ModalStore.tsx
@@ -1,6 +1,27 @@
 // Libs
 import type { ReactElement } from "react";
 import { create } from "zustand";
+import BaseModal from "@/components/Modals/BaseModal";
+
+const modalAvisoForaDoAr = (
+    <BaseModal.Container>
+        <BaseModal.Content
+            modalTitle="Aviso!"
+            modalDescription={
+                <div className="flex flex-col">
+                    <p>
+                        Estamos temporariamente fora do ar 🚧
+                        <br/><br/>
+                        Nosso site está passando por uma instabilidade devido ao alto volume de acessos.
+                        Mas não se preocupe, nossa equipe já está trabalhando para normalizar tudo o mais rápido possível.
+                        <br/><br/>
+                        Agradecemos imensamente o apoio e a compreensão de todos! 💙
+                    </p>
+                </div>
+            }
+        />
+    </BaseModal.Container>
+);
 
 interface ModalStoreProps {
     modal: ReactElement | null;
@@ -13,12 +34,11 @@ interface ModalStoreProps {
 }
 
 const ModalStore = create<ModalStoreProps>((set) => ({
-    modal: null,
+    modal: modalAvisoForaDoAr,
     confirmationModal: null,
 
     addModal: (modal) => set(() => ({ modal })),
-    addConfirmationModal: (confirmationModal) =>
-        set(() => ({ confirmationModal })),
+    addConfirmationModal: (confirmationModal) => set(() => ({ confirmationModal })),
     removeModal: () =>
         set({
             modal: null,
